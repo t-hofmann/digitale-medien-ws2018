263903826115e7f29eb44b4e4c93c70eceb1075a
Thomas Hofmann <thomas.hofmann@hs-anhalt.de>
2018-11-15 09:49:26 +0100

    mindmaps for Linux and Web Technologien, and web-tech-primer implemented with web-tech

---
+++ documentation/Web Technologien.mm
@@ -0,0 +1,265 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1542120050540" ID="ID_1428184220" MODIFIED="1542120844053" TEXT="Web Technologien">
+<node CREATED="1542124173787" ID="ID_1002983073" LINK="https://de.wikipedia.org/wiki/Geschichte_des_Internets" MODIFIED="1542214084675" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Geschichte
+    </p>
+    <p>
+      des Internet
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1542124765660" ID="ID_1688313019" LINK="https://de.wikipedia.org/wiki/Chronologie_des_Internets" MODIFIED="1542214087283" TEXT="Chronologie">
+<node CREATED="1542124994013" ID="ID_78144063" MODIFIED="1542214110541" TEXT="1945">
+<node CREATED="1542125000026" ID="ID_1863243835" LINK="https://de.wikipedia.org/wiki/Vannevar_Bush" MODIFIED="1542125084751" TEXT="Vannevar Bush &quot;As we may think&quot; (Essay)">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      (Entwurf einer fiktiven Maschine &quot;Memory Extender&quot; (memex)
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1542125377476" ID="ID_1898245869" LINK="https://de.wikipedia.org/wiki/Hypertext" MODIFIED="1542125430051" TEXT="Projekt &quot;Xanadu&quot;, Ted Nelson: Hypertext"/>
+</node>
+<node CREATED="1542124307336" ID="ID_283485598" LINK="https://de.wikipedia.org/wiki/Arpanet" MODIFIED="1542214263821" TEXT="1960er/1970er">
+<node CREATED="1542124590980" ID="ID_1969966051" LINK="https://de.wikipedia.org/wiki/Arpanet" MODIFIED="1542124600327" TEXT="ARPANET"/>
+</node>
+<node CREATED="1542124790131" ID="ID_324666023" MODIFIED="1542214326464" TEXT="1973">
+<node CREATED="1542124795275" ID="ID_554534655" MODIFIED="1542125618474" TEXT="frz. Projekt CYCLADES: TCP (fr&#xfc;he Version)"/>
+</node>
+<node CREATED="1542126436545" ID="ID_653784606" MODIFIED="1542214376131" TEXT="1976">
+<node CREATED="1542126441041" ID="ID_1643705627" LINK="https://de.wikipedia.org/wiki/Unix_to_Unix_Copy" MODIFIED="1542126478192" TEXT="UUCP (Unix to Unix Copy Protocol)"/>
+</node>
+<node CREATED="1542125234999" ID="ID_384265185" MODIFIED="1542214379216" TEXT="1977">
+<node CREATED="1542125242327" ID="ID_1186048752" MODIFIED="1542214438748" TEXT="fr&#xfc;he TCP-Version wird aufgespalten:">
+<node CREATED="1542125304278" ID="ID_1474072548" MODIFIED="1542125305522" TEXT="IP"/>
+<node CREATED="1542125307678" ID="ID_1489986692" MODIFIED="1542125309273" TEXT="TCP"/>
+</node>
+<node CREATED="1542125574103" ID="ID_1948218178" MODIFIED="1542125648761" TEXT="ARPANET: 111 Knoten"/>
+</node>
+<node CREATED="1542125164657" ID="ID_1805773451" MODIFIED="1542214380231" TEXT="1979">
+<node CREATED="1542125168184" ID="ID_1453529479" LINK="https://de.wikipedia.org/wiki/Usenet" MODIFIED="1542125173141" TEXT="Usenet"/>
+</node>
+<node CREATED="1542124615879" ID="ID_467294994" MODIFIED="1542214380991" TEXT="ab Ende 1970er">
+<node CREATED="1542124625255" ID="ID_1423910052" MODIFIED="1542124661258" TEXT="akademische, internationale Verbreitung"/>
+</node>
+<node CREATED="1542125628878" ID="ID_1962652405" MODIFIED="1542214381823" TEXT="1983">
+<node CREATED="1542125633133" ID="ID_1964896728" MODIFIED="1542125642065" TEXT="ARPANET: 400 Knoten"/>
+</node>
+<node CREATED="1542125558719" ID="ID_1791246017" MODIFIED="1542214382391" TEXT="1984">
+<node CREATED="1542125563015" ID="ID_1609419033" MODIFIED="1542125564739" TEXT="DNS"/>
+</node>
+<node CREATED="1542125829720" ID="ID_1987773028" MODIFIED="1542214382852" TEXT="1985">
+<node CREATED="1542125833512" FOLDED="true" ID="ID_770724798" LINK="https://de.wikipedia.org/wiki/Mailbox_(Computer)" MODIFIED="1542133767430">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Mailbox-Systeme
+    </p>
+    <p>
+      aka. Bulletin Board System, BBS
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1542126367371" ID="ID_1846517122" LINK="https://de.wikipedia.org/wiki/FidoNet" MODIFIED="1542126376754" TEXT="FidoNet"/>
+<node CREATED="1542126391818" ID="ID_1409134855" LINK="https://de.wikipedia.org/wiki/MausNet" MODIFIED="1542126404180" TEXT="MausNet"/>
+</node>
+</node>
+<node CREATED="1542125798433" ID="ID_1294174061" MODIFIED="1542214609086" TEXT="1986">
+<node CREATED="1542125803633" ID="ID_285195529" MODIFIED="1542125810693" TEXT="DE-NIC"/>
+</node>
+<node CREATED="1542127784295" ID="ID_1703302023" MODIFIED="1542214383591" TEXT="1987">
+<node CREATED="1542127790158" ID="ID_1530506808" MODIFIED="1542127801946" TEXT="Internet: 27.000 Rechner"/>
+</node>
+<node CREATED="1542123202396" ID="ID_828372665" MODIFIED="1542214384944" TEXT="1990">
+<node CREATED="1542123208100" ID="ID_1546936731" MODIFIED="1542125717070">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Tim Berners-Lee
+    </p>
+    <p>
+      &amp; Robert Cailliau
+    </p>
+    <p>
+      (CERN)
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1542123234523" FOLDED="true" ID="ID_1862107162" LINK="https://de.wikipedia.org/wiki/Hypertext_Transfer_Protocol" MODIFIED="1542133778901" TEXT="HTTP">
+<node CREATED="1542123424551" FOLDED="true" ID="ID_1767406510" LINK="https://de.wikipedia.org/wiki/Hypertext_Transfer_Protocol#HTTP-Anfragemethoden" MODIFIED="1542128547661" TEXT="Anfragemethoden">
+<node CREATED="1542123436278" ID="ID_1439375132" MODIFIED="1542123486155" TEXT="GET">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Anfordern einer Resource
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1542123437878" ID="ID_375165651" MODIFIED="1542123522071" TEXT="POST">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Daten
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1542123441526" ID="ID_1789550013" MODIFIED="1542123442866" TEXT="HEAD"/>
+<node CREATED="1542123443342" ID="ID_1231717302" MODIFIED="1542123444738" TEXT="PUT"/>
+<node CREATED="1542123445262" ID="ID_1916528243" MODIFIED="1542123448050" TEXT="PATCH"/>
+<node CREATED="1542123448790" ID="ID_1612399596" MODIFIED="1542123450370" TEXT="DELETE"/>
+<node CREATED="1542123450902" ID="ID_1934717008" MODIFIED="1542123454090" TEXT="TRACE"/>
+<node CREATED="1542123454662" ID="ID_537573330" MODIFIED="1542123456898" TEXT="OPTIONS"/>
+<node CREATED="1542123457582" ID="ID_176567545" MODIFIED="1542123459673" TEXT="CONNECT"/>
+</node>
+</node>
+<node CREATED="1542123240411" FOLDED="true" ID="ID_246513763" LINK="https://de.wikipedia.org/wiki/Uniform_Resource_Identifier" MODIFIED="1542133778238" TEXT="URI">
+<node CREATED="1542128526460" ID="ID_79201527" LINK="https://de.wikipedia.org/wiki/Uniform_Resource_Identifier#Aufbau" MODIFIED="1542128535758" TEXT="Aufbau/Syntax"/>
+</node>
+<node CREATED="1542123242043" ID="ID_638302390" LINK="https://de.wikipedia.org/wiki/Hypertext_Markup_Language" MODIFIED="1542128566806" TEXT="HTML"/>
+</node>
+</node>
+<node CREATED="1542126621396" ID="ID_1477308757" MODIFIED="1542214385414" TEXT="1997">
+<node CREATED="1542126625316" ID="ID_1834940240" MODIFIED="1542126640032" TEXT="Internet: 6 Mio. Knoten"/>
+</node>
+</node>
+</node>
+<node CREATED="1542121922901" ID="ID_1382674499" LINK="https://de.wikipedia.org/wiki/Internetprotokollfamilie" MODIFIED="1542184796043" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Internet-
+    </p>
+    <p>
+      Protokoll-
+    </p>
+    <p>
+      Familie
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1542124224914" ID="ID_1301713377" MODIFIED="1542187727370" TEXT="Schichten">
+<node CREATED="1542122014187" ID="ID_441555610" MODIFIED="1542187728618" TEXT="Anwendungen">
+<node CREATED="1542120589328" ID="ID_1636026450" LINK="https://de.wikipedia.org/wiki/Hypertext_Transfer_Protocol" MODIFIED="1542121738550" TEXT="HTTP"/>
+<node CREATED="1542120592775" ID="ID_1845353471" LINK="https://de.wikipedia.org/wiki/Domain_Name_System" MODIFIED="1542121719178" TEXT="DNS"/>
+<node CREATED="1542120595679" ID="ID_1041527721" LINK="https://de.wikipedia.org/wiki/Simple_Mail_Transfer_Protocol" MODIFIED="1542121805707" TEXT="SMTP"/>
+<node CREATED="1542121817720" ID="ID_36238968" LINK="https://de.wikipedia.org/wiki/Secure_Shell" MODIFIED="1542121833242" TEXT="SSH"/>
+</node>
+<node CREATED="1542122011747" FOLDED="true" ID="ID_742009009" MODIFIED="1542184838818" TEXT="Transport">
+<node CREATED="1542122072001" ID="ID_899889252" LINK="https://de.wikipedia.org/wiki/Transmission_Control_Protocol" MODIFIED="1542131049510" TEXT="TCP - Transmission Control Protocol"/>
+<node CREATED="1542122075089" ID="ID_1889109298" LINK="https://de.wikipedia.org/wiki/User_Datagram_Protocol" MODIFIED="1542131058911" TEXT="UDP - User Datagram Protocol"/>
+<node CREATED="1542122077569" ID="ID_790053968" LINK="https://de.wikipedia.org/wiki/Stream_Control_Transmission_Protocol" MODIFIED="1542131038335" TEXT="SCTP - Stream Control Transmission Protocol"/>
+</node>
+<node CREATED="1542122009435" FOLDED="true" ID="ID_1554614915" MODIFIED="1542184833594" TEXT="Internet">
+<node CREATED="1542122058330" ID="ID_1129143032" LINK="https://de.wikipedia.org/wiki/Internet_Protocol" MODIFIED="1542131190243" TEXT="IP - Internet Protocol (v4, v6)"/>
+<node CREATED="1542122067169" ID="ID_1604522231" LINK="https://de.wikipedia.org/wiki/Internet_Control_Message_Protocol" MODIFIED="1542131208978" TEXT="ICMP - Internet Control Message Protocol"/>
+</node>
+<node CREATED="1542121997579" FOLDED="true" ID="ID_1166144578" MODIFIED="1542184822587" TEXT="Netzzugang">
+<node CREATED="1542122036778" ID="ID_700860116" MODIFIED="1542122044062" TEXT="Ethernet"/>
+<node CREATED="1542122045050" ID="ID_642784288" MODIFIED="1542122052286" TEXT="Token Ring"/>
+<node CREATED="1542122052714" ID="ID_8742336" MODIFIED="1542122053750" TEXT="..."/>
+</node>
+</node>
+<node CREATED="1542120083100" ID="ID_958233051" MODIFIED="1542185670985" TEXT="Socket">
+<node CREATED="1542189709222" ID="ID_1328880626" MODIFIED="1542189738471" TEXT="IP, Port, Protokoll(TCP, UDP)"/>
+<node CREATED="1542129858746" ID="ID_213957896" LINK="https://de.wikipedia.org/wiki/Socket_(Software)#BSD_Sockets_API" MODIFIED="1542185673359" TEXT="BSD-Sockets">
+<node CREATED="1542129961807" FOLDED="true" ID="ID_1919198743" MODIFIED="1542189696362" TEXT="Server">
+<node CREATED="1542129625216" ID="ID_182236109" MODIFIED="1542130257646" TEXT="sockfd = socket(domain, type, protocol)"/>
+<node CREATED="1542129718405" ID="ID_282961793" MODIFIED="1542129908740" TEXT="bind(sockfd, IP, PORT)"/>
+<node CREATED="1542129950271" ID="ID_1765139346" MODIFIED="1542129960691" TEXT="listen()"/>
+<node CREATED="1542130008046" ID="ID_1606154017" MODIFIED="1542130055825" TEXT="com_socket = accept()"/>
+<node CREATED="1542130088676" ID="ID_393667953" MODIFIED="1542130095704" TEXT="close()"/>
+</node>
+<node CREATED="1542129967015" FOLDED="true" ID="ID_1034526081" MODIFIED="1542185803579" TEXT="Client">
+<node CREATED="1542129970511" ID="ID_125946395" MODIFIED="1542129990594" TEXT="connect(IP, PORT)"/>
+<node CREATED="1542130096795" ID="ID_478705584" MODIFIED="1542130100071" TEXT="close()"/>
+</node>
+<node CREATED="1542130385572" FOLDED="true" ID="ID_1271506637" MODIFIED="1542185780842" TEXT="Kommunikation">
+<node CREATED="1542130339309" ID="ID_139391970" MODIFIED="1542130370856" TEXT="send(), write(), sentto()"/>
+<node CREATED="1542130343165" ID="ID_901415902" MODIFIED="1542130384176" TEXT="rcv(), read(), recvfrom()"/>
+</node>
+</node>
+<node CREATED="1542130121259" ID="ID_805406624" LINK="https://en.wikipedia.org/wiki/Berkeley_sockets" MODIFIED="1542185771480" TEXT="mehr Details &quot;Berkeley Sockets&quot; (ENG)"/>
+<node CREATED="1542120092180" ID="ID_339443201" LINK="https://en.wikipedia.org/wiki/Ephemeral_port" MODIFIED="1542121062193" TEXT="Ephemeral Ports"/>
+</node>
+</node>
+<node CREATED="1542120509106" ID="ID_1134794035" MODIFIED="1542187760379" POSITION="right" TEXT="WWW-Standards">
+<node CREATED="1542120512154" ID="ID_618641968" LINK="https://de.wikipedia.org/wiki/Hypertext_Markup_Language" MODIFIED="1542217379417" TEXT="HTML">
+<node CREATED="1542128920594" ID="ID_1107821589" LINK="https://de.wikipedia.org/wiki/Hypertext_Markup_Language#Syntax" MODIFIED="1542128945821" TEXT="Syntax"/>
+<node CREATED="1542128924873" ID="ID_1944496706" LINK="https://de.wikipedia.org/wiki/Hypertext_Markup_Language#HTML-Struktur" MODIFIED="1542128937230" TEXT="Struktur"/>
+<node CREATED="1542129029687" ID="ID_151069668" LINK="https://de.wikipedia.org/wiki/HTML5" MODIFIED="1542129035080" TEXT="HTML5"/>
+</node>
+<node CREATED="1542120514993" FOLDED="true" ID="ID_1102998560" LINK="https://de.wikipedia.org/wiki/Cascading_Style_Sheets" MODIFIED="1542218364237" TEXT="CSS">
+<node CREATED="1542134329495" ID="ID_1117260553" LINK="https://de.wikipedia.org/wiki/Cascading_Style_Sheets#Syntax" MODIFIED="1542134335803" TEXT="Syntax"/>
+<node CREATED="1542134343070" ID="ID_1137407884" MODIFIED="1542134348546" TEXT="Selektoren"/>
+</node>
+<node CREATED="1542120516833" ID="ID_780177136" MODIFIED="1542120520526" TEXT="Javascript"/>
+<node CREATED="1542189551330" ID="ID_256283359" LINK="https://en.wikipedia.org/wiki/Http" MODIFIED="1542217376614" TEXT="HTTP">
+<node CREATED="1542189374286" ID="ID_1467159612" LINK="https://tools.ietf.org/html/rfc2068" MODIFIED="1542189616140" TEXT="HTTP/1.1 (RFC 2068)"/>
+<node CREATED="1542189367383" ID="ID_705006159" LINK="https://tools.ietf.org/html/rfc7540" MODIFIED="1542189622156" TEXT="HTTP/2 (RFC 7540)"/>
+</node>
+</node>
+<node CREATED="1542120454547" ID="ID_665864130" MODIFIED="1542215791621" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Standardization
+    </p>
+    <p>
+      Organizations
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1542120464747" ID="ID_1708811197" LINK="https://de.wikipedia.org/wiki/Internationale_Organisation_f%C3%BCr_Normung" MODIFIED="1542216812085" TEXT="ISO - International Organization for Standardization"/>
+<node CREATED="1542120467339" ID="ID_1236905942" LINK="https://de.wikipedia.org/wiki/Internet_Engineering_Task_Force" MODIFIED="1542216294118" TEXT="IETF - Internet Engineering Task Force">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      RFCs
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1542120484570" ID="ID_130867825" LINK="https://de.wikipedia.org/wiki/World_Wide_Web_Consortium" MODIFIED="1542216784058" TEXT="w3c - World Wide Web Consortium"/>
+<node CREATED="1542126666755" ID="ID_1218399261" LINK="https://de.wikipedia.org/wiki/Internet_Corporation_for_Assigned_Names_and_Numbers" MODIFIED="1542215908160" TEXT="ICANN - Internet Corporation for Assigned Names and Numbers">
+<node CREATED="1542125676220" ID="ID_629858869" LINK="https://de.wikipedia.org/wiki/Internet_Assigned_Numbers_Authority" MODIFIED="1542216978574" TEXT="IANA - Intetnet Assigned Numbers Authority">
+<node CREATED="1542127719192" ID="ID_504204286" LINK="https://de.wikipedia.org/wiki/Jon_Postel" MODIFIED="1542127728088" TEXT="Jon Postel"/>
+</node>
+</node>
+</node>
+</node>
+</map>
